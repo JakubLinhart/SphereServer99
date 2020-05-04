6da29f23a3fe9633344f3720ff868e53ff91bf27
Jakub Linhart <jakub.linhart@gmail.com>
2020-05-05 00:26:05 +0200

    fix infusion test start

--- tests/infusion/sphere56.cmd
+++ tests/infusion/sphere56.cmd
@@ -1,7 +1,9 @@
-IF NOT EXIST "..\paket\packages\Infusion.Cli.Tools\tools\Infusion.Cli.exe" (
+@IF NOT EXIST "..\paket\packages\Infusion.Cli.Tools\tools\Infusion.Cli.exe" (
     pushd .
     cd ..\paket
     paket.exe restore
     popd
 )
-..\paket\packages\Infusion.Cli.Tools\tools\Infusion.Cli.exe hl --server localhost --serverPort 2595 --proxyPort 60001 --clientVersion 3.0.6 --account drzhor --password password --shard MyShard --char DrZhor --script scripts\tests\%1
+
+set INFUSION_PATH=%CD%
+..\paket\packages\Infusion.Cli.Tools\tools\Infusion.Cli.exe hl --server localhost --serverPort 2595 --proxyPort 60001 --clientVersion 3.0.6 --account drzhor --password password --shard MyShard --char DrZhor --script %INFUSION_PATH%\scripts\tests\%1
